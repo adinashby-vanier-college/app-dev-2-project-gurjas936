a049a703718a0f11918c1902ce3e0f6acab34f53
Simranjeet Kaur <2102116@edu.vaniercollege.qc.ca>
2022-11-09 23:52:14 -0500

    navigation bar .

--- lib/screens/navBar.dart
+++ lib/screens/navBar.dart
@@ -6,7 +6,29 @@ class NavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
         child: ListView(
-        children: [],
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('abc'),
+            accountEmail: Text('abc@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              child: ClipOval(
+                child: Image.network(
+                  'https://cdn.pixabay.com/photo/2017/02/25/22/04/user-icon-2098873_960_720.png',
+                  fit: BoxFit.cover,
+                  width: 90,
+                  height: 90,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage('https://media.istockphoto.com/id/1252380604/photo/note-paper-with-blood-donation-concept-on-red-background.jpg?b=1&s=170667a&w=0&k=20&c=HJpGKpSF3ULIBECqzb6vr2zT5dx-dhbGmbOoI5XsRxo=')
+              ),
+            ),
+          ),
+        ],
         ),
 
 
