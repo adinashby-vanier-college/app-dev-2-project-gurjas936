7bf8911507f4559a3536fc74a0e12163da82cb4d
gurjas936 <singhgurjas936@gmail.com>
2022-11-22 17:37:47 -0500

    toggle switch added

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Admin\StudioProjects\blood_bank"
+export "FLUTTER_APPLICATION_PATH=C:\vanier\sem5\applicationDev\project"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- lib/screens/donateBlood.dart
+++ lib/screens/donateBlood.dart
@@ -17,95 +17,101 @@ class _donateBloodState extends State<donateBlood> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: const Color(0xffe72041),
-      body: Stack(children: [
-        Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Expanded(
-            child: Column(
-              children: [
-                Container(
-                  height: 260,
-                  width: 250,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('images/quat1.png'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.end,
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 260,
+                      width: 250,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('images/quat1.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 30),
-                            child: Container(
-                              height: 70,
-                              width: 70,
-                              decoration: const BoxDecoration(
-                                image: DecorationImage(
-                                  image: AssetImage('images/pp.jpg'),
-                                  fit: BoxFit.cover,
-                                ),
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(60.0)),
-                              ),
-                            ),
-                          ),
-                          Column(
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.end,
                             children: [
-                              Container(
-                                child: const Text(
-                                  'Blood Type:',
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w900,
-                                      color: Colors.white),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 30),
+                                child: Container(
+                                  height: 70,
+                                  width: 70,
+                                  decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage('images/pp.jpg'),
+                                      fit: BoxFit.cover,
+                                    ),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(60.0)),
+                                  ),
                                 ),
                               ),
-                              Container(
-                                child: const Text(
-                                  'Age:',
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w900,
-                                      color: Colors.white),
-                                ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    child: const Text(
+                                      'Blood Type:',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w900,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                  Container(
+                                    child: const Text(
+                                      'Age:',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w900,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ],
                           ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 45, bottom: 40),
+                            child: Container(
+                              alignment: Alignment.centerLeft,
+                              child: const Text(
+                                'abc',
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w900,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          )
                         ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 45, bottom: 40),
-                        child: Container(
-                          alignment: Alignment.centerLeft,
-                          child: const Text(
-                            'abc',
-                            style: TextStyle(
-                                fontSize: 25,
-                                fontWeight: FontWeight.w900,
-                                color: Colors.white),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ],
+                    ),
+                  ],
                 ),
               ),
-          Expanded(
-            flex: 2,
-            child: Container(color: Colors.white,),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.white,
+                ),
+              ),
+            ],
           ),
-
-
-        ],),
-      ],),
+        ],
+      ),
     );
   }
 }
